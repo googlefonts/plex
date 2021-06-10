92661fc8d5d82fe686fd35775ea2f0819f968bb5
Yanone <post@yanone.de>
2021-06-10 15:23:41 +0200

    Use shortened postscript name, drop all Mac name records

--- Google-Fonts-Fixes/scripts/convert.py
+++ Google-Fonts-Fixes/scripts/convert.py
@@ -12,6 +12,10 @@ if not os.path.exists(outputfolder):
 filename = os.path.basename(path)
 outputpath = os.path.join(outputfolder, filename)
 postScriptName = os.path.splitext(filename)[0]
+
+# Shorten
+postScriptName = postScriptName.replace("Condensed", "Cond")
+
 postScriptFamilyname, postScriptStylename = postScriptName.split("-")
 
 
@@ -31,6 +35,7 @@ else:
 
 print(familyName, styleName)
 
+# Complete implementation of https://github.com/googlefonts/gf-docs/tree/main/Spec#supported-styles
 map = {
     "upright": {
         100: {
@@ -267,73 +272,9 @@ for record in ttFont["name"].names:
 ttFont["name"].setName(f"{familyName} {styleName}", 4, 1, 0, 0)
 ttFont["name"].setName(f"{familyName} {styleName}", 4, 3, 1, 0x409)
 
-
-# # Copyright Notice
-# ttFont["name"].setName(
-#     "Copyright 2018 The IBM Plex Project Authors (https://github.com/ibm/plex/)",
-#     0,
-#     1,
-#     0,
-#     0,
-# )
-# ttFont["name"].setName(
-#     "Copyright 2018 The IBM Plex Project Authors (https://github.com/ibm/plex/)",
-#     0,
-#     3,
-#     1,
-#     0x409,
-# )
-
-
-# # Rename version from 3.2 to 3.002 in name table
-# head_version = fractions.Fraction(ttFont["head"].fontRevision)
-# version_extended = str(float(f"{float(head_version):.5f}"))
-# version_contracted = ".".join(
-#     [str(x) for x in [int(x) for x in version_extended.split(".")]]
-# )
-# for record in ttFont["name"].names:
-#     ttFont["name"].setName(
-#         str(
-#             ttFont["name"].getName(
-#                 record.nameID, record.platformID, record.platEncID, record.langID
-#             )
-#         ).replace(version_contracted, version_extended),
-#         record.nameID,
-#         record.platformID,
-#         record.platEncID,
-#         record.langID,
-#     )
-
-
-# # Use Typographic Family Name as Family Name
-# if ttFont["name"].getName(16, 1, 0, 0):
-#     familyName = ttFont["name"].getName(16, 1, 0, 0).toUnicode()
-#     ttFont["name"].setName(familyName, 1, 1, 0, 0)
-#     ttFont["name"].setName(familyName, 1, 3, 1, 0x409)
-#     ttFont["name"].removeNames(16, 1, 0, 0)
-#     ttFont["name"].removeNames(16, 3, 1, 0x409)
-# if ttFont["name"].getName(17, 1, 0, 0):
-#     styleName = ttFont["name"].getName(17, 1, 0, 0).toUnicode()
-#     ttFont["name"].setName(styleName, 2, 1, 0, 0)
-#     ttFont["name"].setName(styleName, 2, 3, 1, 0x409)
-#     ttFont["name"].removeNames(17, 1, 0, 0)
-#     ttFont["name"].removeNames(17, 3, 1, 0x409)
-
-# # Set PostScript name
-# ttFont["name"].setName(postScriptName, 6, 1, 0, 0)
-# ttFont["name"].setName(postScriptName, 6, 3, 1, 0x409)
-
-# Unused, just for reference
-# ttFont["head"].macStyle |= 1 << 1  # Set Italic bit
-# ttFont["OS/2"].fsSelection &= ~(1 << 5)  # Clear Bold bit
-# ttFont["OS/2"].fsSelection &= ~(1 << 6)  # Clear Regular bit
-# ttFont["OS/2"].fsSelection |= 1 << 0  # Set Italic bit
-
-# fullName = (
-#     str(ttFont["name"].getName(1, 1, 0, 0))
-#     + " "
-#     + str(ttFont["name"].getName(2, 1, 0, 0))
-# )
-
+# Sad but true, drop all Mac names
+for record in ttFont["name"].names:
+    if record.platformID == 1:
+        ttFont["name"].removeNames(record.nameID, 1, 0, 0)
 
 ttFont.save(outputpath)
