20f93381c40c4779f2297bb31c60fbc2992620b6
Yanone <post@yanone.de>
2021-06-18 15:22:13 +0200

    Readme, remove *all

--- Google-Fonts-Fixes/scripts/convert-all.sh
+++
@@ -1,4 +0,0 @@
-sh Google-Fonts-Fixes/scripts/convert-sans.sh
-sh Google-Fonts-Fixes/scripts/convert-sans-condensed.sh
-sh Google-Fonts-Fixes/scripts/convert-mono.sh
-sh Google-Fonts-Fixes/scripts/convert-serif.sh
