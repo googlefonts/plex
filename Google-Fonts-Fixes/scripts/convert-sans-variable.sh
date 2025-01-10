3e312890b3b9e47378b30dedfe4196a42151243c
Yanone <post@yanone.de>
2025-01-10 13:39:49 +0100

    Discard static Sans entirely, pretend Var Sans is the only Sans moving forward

--- Google-Fonts-Fixes/scripts/convert-sans-variable.sh
+++
@@ -1,1 +0,0 @@
-sh Google-Fonts-Fixes/scripts/convert.sh "packages/plex-sans-variable/fonts/complete/ttf" "Google-Fonts-Fixes/fonts/IBM-Plex-Sans-Variable/fonts/complete/ttf"
--- Google-Fonts-Fixes/scripts/convert-sans.sh
+++ Google-Fonts-Fixes/scripts/convert-sans.sh
@@ -1,1 +1,1 @@
-sh Google-Fonts-Fixes/scripts/convert.sh "packages/plex-sans/fonts/complete/ttf" "Google-Fonts-Fixes/fonts/IBM-Plex-Sans/fonts/complete/ttf"
+sh Google-Fonts-Fixes/scripts/convert.sh "packages/plex-sans-variable/fonts/complete/ttf" "Google-Fonts-Fixes/fonts/IBM-Plex-Sans/fonts/complete/ttf"
